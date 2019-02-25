cd18b945c15a3dd37d491f36600c6856f584dd72
Adam Whitlock <adam.whitlock627@gmail.com>
2019-02-24 21:48:01 -0700

    moved my branch to use mongoBB atlas rolled back to use pug for now

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 const session = require('express-session');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user.model.js');
+require('dotenv').config();
 
 const userRoutes = require('./routes/user.route');
 const frameRoutes = require('./routes/frame.route');
@@ -14,25 +15,23 @@ const pageRoutes = require('./routes/page.route');
 const errorHandler = require('./error');
 const app = express();
 
-
 //db connection
 const mongoose = require('mongoose');
-let db_url= process.env.DBURL;
-mongoose.connect(db_url);
+let db_url = process.env.DBURL;
+mongoose.connect(db_url, { dbName: process.env.DBCOLLECTION });
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
+// db.on('connection', console.log('DB connected successfully'));
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 //middleware
 app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(fileUpload());
-app.use(session({secret: process.env.SESSIONSECRET}));
+app.use(session({ secret: process.env.SESSIONSECRET }));
 app.use(passport.initialize());
 app.use(passport.session());
-
 
 app.use('/users', userRoutes);
 app.use('/frames', frameRoutes);
@@ -40,27 +39,29 @@ app.use('/static', otherRoutes);
 app.use(pageRoutes);
 app.use(errorHandler);
 
+passport.use(
+	new LocalStrategy(
+		{
+			usernameField: 'name',
+			passwordField: 'password'
+		},
+		function(username, password, done) {
+			User.findOne({ name: username }, function(err, user) {
+				if (err) {
+					return done(err);
+				}
+				if (!user || !user.validatePassword(password)) {
+					return done(null, false, { message: 'incorrect username/password' });
+				}
 
-passport.use(new LocalStrategy({
-	usernameField: 'name',
-	passwordField: 'password',
-},
-function(username, password, done){
-	User.findOne({name:username}, function(err, user){
-
-		if(err) {
-			return done(err);
+				return done(null, user);
+			});
 		}
-		if(!user || !user.validatePassword(password)){
-			return done(null, false, {message: 'incorrect username/password'});
-		}
-
-		return done(null, user);
-	});
-}));
+	)
+);
 
 passport.serializeUser(function(user, done) {
-	done(null,{username: user.name, id: user._id});
+	done(null, { username: user.name, id: user._id });
 });
 
 passport.deserializeUser(function(user, done) {
@@ -69,10 +70,12 @@ passport.deserializeUser(function(user, done) {
 	});
 });
 
-app.post('/login', passport.authenticate('local', {
-	successRedirect: '/',
-	failureRedirect: '/login',
-})
+app.post(
+	'/login',
+	passport.authenticate('local', {
+		successRedirect: '/',
+		failureRedirect: '/login'
+	})
 );
 
 let port = 8080;
